106fdb403e14257712ef321a169541039a3f32cb
EmiVillarruel <103389269+EmiVillarruel@users.noreply.github.com>
2022-07-27 21:17:16 -0300

    Completando HTML, CSS del header

--- ProyectoArgPro/src/app/app.module.ts
+++ ProyectoArgPro/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IlustracionComponent } from './components/ilustracion/ilustracion.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { LogosComponent } from './components/logos/logos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IlustracionComponent,
+    PerfilComponent,
+    LogosComponent
   ],
   imports: [
     BrowserModule,
